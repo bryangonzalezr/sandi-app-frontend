b3a27190be7968b895d1160e42d651251c35db43
nrojel <nicole.rojelg@gmail.com>
2024-06-19 22:58:49 -0400

    ADD: Tailwind y Pinia

--- sandi_app/src/router/index.ts
+++ sandi_app/src/router/index.ts
@@ -1,31 +1,22 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import TabsPage from '../views/TabsPage.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/tabs/tab1'
-  },
-  {
-    path: '/tabs/',
-    component: TabsPage,
+    component: () => import('@/views/HomePage.vue'),
     children: [
       {
         path: '',
-        redirect: '/tabs/tab1'
+        redirect: '/home'
       },
       {
-        path: 'tab1',
-        component: () => import('@/views/Tab1Page.vue')
+        path: 'home',
+        component: () => import('@/components/StartPage.vue')
       },
       {
-        path: 'tab2',
-        component: () => import('@/views/Tab2Page.vue')
-      },
-      {
-        path: 'tab3',
-        component: () => import('@/views/Tab3Page.vue')
+        path: 'chat',
+        component: () => import('@/components/ChatPage.vue')
       }
     ]
   }
---
+++ sandi_app/src/views/HomePage.vue
@@ -0,0 +1,23 @@
+<template>
+  <ion-page>
+    <ion-tabs>
+      <ion-router-outlet></ion-router-outlet>
+      <ion-tab-bar slot="bottom">
+        <ion-tab-button tab="tab1" href="/home">
+          <ion-icon aria-hidden="true" :icon="triangle" />
+          <ion-label>Inicio</ion-label>
+        </ion-tab-button>
+      </ion-tab-bar>
+    </ion-tabs>
+    <ion-fab vertical="bottom" horizontal="end">
+    <ion-fab-button href="/chat">
+      <ion-icon class="text-white" :icon="chatboxEllipsesOutline"></ion-icon>
+      </ion-fab-button>
+    </ion-fab>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
+import { chatboxEllipsesOutline, triangle } from 'ionicons/icons';
+</script>
