8439fad51e50b1134d912607c489f5e77e1053dd
Benjamin Campos <benjamin.campos@neering.cl>
2024-11-14 22:06:47 -0300

    FIX recipes without instructions

--- sandi_app/src/stores/recipe.js
+++ sandi_app/src/stores/recipe.js
@@ -54,7 +54,7 @@ export const useRecipeStore = defineStore('recipe', {
           'label': recipe.label,
           'ingredientLines': recipe.ingredientLines,
           'ingredients': recipe.ingredients,
-          'instruction': recipe.instructions,
+          'instructions': recipe.instructions,
           'calories': recipe.calories,
           'sandi_recipe': sandi_recipe,
           'user_id': id_patient,
